7ccf844d6e87909039bacc95527b5d6044ad2886
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-09 10:13:07 +0200

    Hero + About section complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { SectionWrapper } from "../hoc";
+
+import { styles } from "../style";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt0-4 text-secondary text-[17px] max-w-3xl leading-[30px] "
+      >
+        I am an interaction designer and developer, who is enthusiastic about
+        the blockchain ecosystem and software development. I work with Front-End
+        technologies, such as HTML, CSS and JavaScript, and I am constantly
+        learning frontend and blockchain technologies to understand secure smart
+        contracts and grow as a UX Developer and Fullstack Javascript/Blockchain
+        developer.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
