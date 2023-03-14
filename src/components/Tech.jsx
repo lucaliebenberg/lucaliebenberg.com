9b3f893314db68779e330f29996b9b291b94f0b4
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-14 13:36:13 +0200

    updated changes to Tech

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,3 +1,29 @@
+// import React from "react";
+// import { BallCanvas } from "./canvas";
+// import { SectionWrapper } from "../hoc";
+// import { technologies } from "../constants";
+
+// const Tech = () => {
+//   return (
+//     <div className="flex flex-row flex-wrap justify-center gap-10">
+//       {technologies.map((technology) => (
+//         <div className="w-28 h-28" key={technology.name}>
+//           <BallCanvas
+//             icon={technology.icon}
+//             controlsProps={{
+//               minDistance: 1,
+//               maxDistance: 10,
+//               minPolarAngle: 0,
+//             }}
+//           />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default SectionWrapper(Tech, "#tech");
+
 import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
