224420b6ecb7b5e321f0cd27582c32932fa4b8fd
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-21 12:35:49 +0200

    updated project description

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -94,8 +94,8 @@ const Works = () => {
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          ability to solve problems, work with different technologies, and
+          manage projects effectively.
         </motion.p>
       </div>
 
