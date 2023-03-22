89744715df21ef5976f84c608332a63142bcfae6
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-22 21:57:36 +0200

    minor change to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Luca</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop user interfaces, <br className="sm:block hidden" /> web
-            apps, and mobile apps.
+            I design && develop <br className="sm:block hidden" />
+            user interfaces & web apps.
           </p>
         </div>
       </div>
