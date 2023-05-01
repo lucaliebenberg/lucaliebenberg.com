442c93a3ff09234b93a477f65550dfa2bd7f6e64
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-05-01 10:03:42 +0200

    changes to resume link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -48,7 +48,7 @@ const Footer = () => {
             <AiFillYoutube size={24} className="mr-3" />
           </a> */}
           <a
-            href="https://i.ibb.co/YTFgSwm/resume.jpg"
+            href="https://i.ibb.co/JjJtZ56/L-Liebenberg-Resume.jpg"
             target={"_blank"}
             className="text-[#CCC] font-semibold hover:text-white hover:scale-105 hover:cursor-pointer hover:transition-all hover:duration-300 hover:ease-in-out"
           >
