e09b2b5102bcb28095d1723a54f787dae19fbed8
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-03-10 10:47:58 +0200

    updated changes to site, removing 3d element for improved page load performance

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,8 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* TODO: improve drastically poor page load */}
+      {/* <ComputersCanvas /> */}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
