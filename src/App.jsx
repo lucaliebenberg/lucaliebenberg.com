56474a3a2151f95b125ed4ae563aa787f3cc05f6
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-08-06 19:58:42 +0200

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,12 @@ const App = () => {
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-cover bg-no-repeat bg-center">
-          {/* <Navbar /> */}
           <Hero />
         </div>
         <About />
         <Experience />
         {/* <Tech /> */}
         <Works />
-        {/* <Feedbacks /> */}
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
