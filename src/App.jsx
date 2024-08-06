917a5efcf30f96455d86b8061c785af3dfcc6e9a
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-08-06 19:58:31 +0200

    bugfix: webGL not rendering correctly

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
         </div>
         <About />
         <Experience />
-        <Tech />
+        {/* <Tech /> */}
         <Works />
         {/* <Feedbacks /> */}
         <div className="relative z-0">
